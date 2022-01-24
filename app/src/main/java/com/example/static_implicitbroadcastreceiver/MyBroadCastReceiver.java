6333de5c28024d2811575614affd35295578da32
usama <codingeyes6@gmail.com>
2022-01-24 18:37:40 +0500

    create broadcast receiver class and define action in manifest

---
+++ app/src/main/java/com/example/static_implicitbroadcastreceiver/MyBroadCastReceiver.java
@@ -0,0 +1,12 @@
+package com.example.static_implicitbroadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyBroadCastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
